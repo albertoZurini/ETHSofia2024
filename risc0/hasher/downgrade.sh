37d63f31e83e691f9218ed5aea9d884007958d5c
albertoZurini <alberto.zurini@gmail.com>
2024-10-18 17:11:43 +0200

    fix: removed useless; added script to downgrade dependencies

---
+++ risc0/hasher/downgrade.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+cargo update risc0-zkvm --precise 1.0.1
+cargo update risc0-build --precise 1.0.1
+cargo update risc0-circuit-rv32im --precise 1.0.1
+cargo update risc0-groth16 --precise 1.0.1
+cargo update risc0-binfmt --precise 1.0.1
+
+cargo update risc0-circuit-recursion --precise 1.0.1
+cargo update risc0-zkp --precise 1.0.1
+cargo update risc0-core --precise 1.0.1
+cargo update risc0-zkvm-platform --precise 1.0.1
+
